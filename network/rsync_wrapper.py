5e820cee04f22aa4b9a95e8bc90049e8d14af986
yu <yul1024@outlook.com>
2026-01-12 21:37:38 +0800

    feat: add rsync wrapper

---
+++ network/rsync_wrapper.py
@@ -0,0 +1,23 @@
+"""
+Source:
+    https://github.com/yuliu625/Yu-CLI-Wrapper/network/rsync_wrapper.py
+
+
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+import subprocess
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class RsyncWrapper:
+    @staticmethod
+    def run_rsync(
+
+    ) -> None:
+        ...
+
