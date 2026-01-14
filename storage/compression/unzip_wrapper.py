9dd7446bc653614f680d0ec5f909406eea83766e
yu <yul1024@outlook.com>
2026-01-14 21:31:45 +0800

    to: add unzip wrapper

--- storage/compression/unzip_wrapper.py
+++ storage/compression/unzip_wrapper.py
@@ -10,3 +10,18 @@ import subprocess
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
+
+class UnzipWrapper:
+    @staticmethod
+    def unzip_file(
+        file_path: str,
+        target_dir: str | None = None,
+    ) -> None:
+        ...
+
+    @staticmethod
+    def check_files(
+        file_path: str,
+    ) -> None:
+        ...
+
