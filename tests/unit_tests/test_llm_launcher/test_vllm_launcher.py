cf4880a080762dbc38935260a9306c6e64d0f518
yu <yul1024@outlook.com>
2026-01-19 23:21:07 +0800

    test: add tests

---
+++ tests/unit_tests/test_llm_launcher/test_vllm_launcher.py
@@ -0,0 +1,26 @@
+"""
+测试VLLM的启动和运行。
+
+因为约定启动的是API服务，因此基于网络进行测试。
+"""
+
+from __future__ import annotations
+import pytest
+
+import subprocess
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+def test_vllm_status():
+    ...
+
+
+def test_vllm_via_curl():
+    ...
+
+
+def test_vllm_via_openai_client():
+    ...
+
