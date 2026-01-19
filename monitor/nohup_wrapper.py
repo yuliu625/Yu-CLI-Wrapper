5a45fbd30b412bc12c5d7ef443a88d8c49af8f64
yu <yul1024@outlook.com>
2026-01-19 23:38:39 +0800

    feat: add nohup wrapper

---
+++ monitor/nohup_wrapper.py
@@ -0,0 +1,28 @@
+"""
+Sources:
+
+References:
+
+Synopsis:
+
+Notes:
+
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class NoHupWrapper:
+    @staticmethod
+    def run(
+
+    ):
+        ...
+
+
+if __name__ == '__main__':
+    ...
+
